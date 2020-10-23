cefb57f5b82821f38544388ff71f2c423c3f6a6b
Ананичев Леонид Дмитриевич <ananichev.l.dm@sberbank.ru>
2020-10-23 18:42:34 +0300

    feat: checkBrackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            console.log(str[i]);
+
+            if (str[i] === bracketsConfig[j][1]) {
+                if (!stack.length) {
+                    if (str[i] === bracketsConfig[j][0]) {
+                        stack.push(j);
+                        console.log('push');
+                        break;
+                    }
+                    return false;
+                }
+
+                if (bracketsConfig[stack[stack.length - 1]][1] === str[i]) {
+                    stack.pop();
+                    console.log('pop');
+                    break;
+                } else {
+                    if (str[i] === bracketsConfig[j][0]) {
+                        stack.push(j);
+                        console.log('push');
+                        break;
+                    }
+                    return false;
+                }
+            }
+
+
+            if (str[i] === bracketsConfig[j][0]) {
+                stack.push(j);
+                console.log('push');
+                break;
+            }
+        }
+
+        console.log(stack);
+    }
+
+    if (stack.length) {
+        return false;
+    }
+
+    return true;
+};
